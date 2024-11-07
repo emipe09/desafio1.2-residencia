a1e03edc47b941072ff15d1c231ea7372a0e183f
Marcos Paulo <mpmarcos1016@gmail.com>
2024-11-07 16:55:57 -0300

    add package, index e parte dos menus

--- Classes/Clinica.js
+++ Classes/Clinica.js
@@ -1,45 +1,64 @@
 import { DateTime, Interval } from 'luxon';
-import { Paciente } from './paciente.js';s
+import { Paciente } from './paciente.js';
 
 export class Clinica{
+    #pacientes = [];
     constructor(nome, endereço, telefone){
         this.nome = nome;
         this.endereço = endereço;
         this.telefone = telefone;
-        this.pacientes = [];
     }
 
     addPaciente(p){
         if(p){
-            this.pacientes.push(p);
+            this.#pacientes.push(p);
         }
     }
 
     removePaciente(cpf){
-        let i = this.pacientes.findIndex(p => p.cpf == cpf);
+        let i = this.#pacientes.findIndex(p => p.cpf == cpf);
         if(i != -1){
-            this.pacientes.splice(i, 1);
+            this.#pacientes.splice(i, 1);
         }
     }
 
     listarPacientesNome(){
-        this.pacientes.sort((a, b) => a.nome.localeCompare(b.nome));
+        this.#pacientes.sort((a, b) => a.nome.localeCompare(b.nome));
         console.log('-------------------------------------');
         console.log('CPF | Nome | Data de Nascimento | Idade');
         console.log('-------------------------------------');
-        this.pacientes.forEach(p => {
+        this.#pacientes.forEach(p => {
             console.log(`${p.cpf} | ${p.nome} | ${p.dataNasc} | ${p.idade}`);
         });
 
     }
 
     listarPacientesCpf(){
-        this.pacientes.sort((a, b) => a.cpf.localeCompare(b.cpf));
+        this.#pacientes.sort((a, b) => a.cpf.localeCompare(b.cpf));
         console.log('-------------------------------------');
         console.log('CPF | Nome | Data de Nascimento | Idade');
         console.log('-------------------------------------');
-        this.pacientes.forEach(p => {
+        this.#pacientes.forEach(p => {
             console.log(`${p.cpf} | ${p.nome} | ${p.dataNasc} | ${p.idade}`);
         });
     }
+
+    agendarConsulta(cpf, data, horaInicial, horaFinal){
+        let i = this.#pacientes.findIndex(p => p.cpf == cpf);
+        if((i != -1)&&(horaFinal>horaInicial)){
+            let dataConsulta = DateTime.fromFormat(data, 'dd/MM/yyyy');
+            //validando horario HHMM
+            if((horaInicial<0)||(horaInicial>2359)||(horaFinal<0)||(horaFinal>2359)){
+                throw new Error("Horário inválido");
+            }
+            if(dataConsulta.isValid){
+                this.#pacientes[i].agendarConsulta(dataConsulta);
+            }
+            else{
+                throw new Error("Data inválida");
+            }
+        }
+    }
+
+
 }
--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Parte principal do código, vai incluir o menu
+//Menu Principal
+
+import promptSync from 'prompt-sync';
+const readline = promptSync();
+
+import { Paciente } from "./Classes/paciente.js"
+import { Clinica } from "./Classes/Clinica.js"
+
+function menuPrincipal(){
+    console.log('\n\n* Menu Principal* \n-------------------------------------------------------------------------------');
+    console.log('1 - Cadastro de Paciente\n2 - Agenda\n3 - Fim');
+    console.log('-------------------------------------------------------------------------------');
+    let i = Number(readline('Escolha uma opção: '));
+    if(i==1){
+        menuPaciente(i);
+    }
+    else if(i==2){
+        menuAgenda(i);
+    }
+    else if(i==3){
+        console.log('Fim');
+    }
+    else{
+        console.log('Opção inválida');
+    }
+}
+
+function menuPaciente(i){
+    console.log('\n\n* Menu do Cadastro de Pacientes *\n-------------------------------------------------------------------------------');
+    console.log("1 - Cadastrar novo paciente\n2 - Excluir paciente\n3 - Listar pacientes por nome\n4 - Listar pacientes por CPF");
+    console.log("5 - Voltar p/ menu principal");
+    console.log('-------------------------------------------------------------------------------');
+
+    let j = Number(readline('Escolha uma opção: '));
+
+}
+
+menuPrincipal();
+
+
